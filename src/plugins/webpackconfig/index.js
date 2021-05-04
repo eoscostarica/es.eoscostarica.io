25d61b62970acea192893e8f145ba22ef5e41289
AngeloCG97 <angelocas13_8@hotmail.com>
2021-05-04 08:30:48 -0600

    fix(deploy): fix deploy bugs

--- src/plugins/webpackconfig/index.js
+++ src/plugins/webpackconfig/index.js
@@ -4,17 +4,9 @@ module.exports = function () {
     name: 'custom-docusaurus-plugin',
     configureWebpack() {
       return {
-        module: {
-          rules: [
-            { 
-                test: /\.(png|woff|woff2|eot|ttf|svg)$/, 
-                loader: 'url-loader?limit=100000'
-            }
-          ],
-        },
         plugins: [
           new webpack.ProvidePlugin({
-              Buffer: ['buffer', 'Buffer'],
+              Buffer: ['buffer', 'Buffer']
           })
         ],
       };
