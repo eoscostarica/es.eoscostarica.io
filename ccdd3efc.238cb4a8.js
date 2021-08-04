6096c17205e58d8fd214b5fd3e307437b7dfc997
AngeloCG97 <51149817+AngeloCG97@users.noreply.github.com>
2021-08-04 14:00:35 +0000

    Deploying to gh-pages from  @ 738aca3c66ead1e41e1820cf455258d15cf435c8 🚀

---
+++ ccdd3efc.238cb4a8.js
@@ -0,0 +1,10 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([[71],{
+
+/***/ 136:
+/***/ (function(module) {
+
+module.exports = JSON.parse("{\"allTagsPath\":\"/blog/tags\",\"slug\":\"nfts\",\"name\":\"nfts\",\"count\":1,\"permalink\":\"/blog/tags/nfts\"}");
+
+/***/ })
+
+}]);
